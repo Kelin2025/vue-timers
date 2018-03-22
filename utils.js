1c83f03c8e01a29d8183496146e3e26d3f8d2f54
Kelin2025 <kelin2025@yandex.ru>
2018-03-22 12:04:28 +0300

    Removed unused utils

--- utils.js
+++ utils.js
@@ -1,13 +1,3 @@
-export function isFunction(cb) {
-  return typeof callback === 'function'
-}
-
-export function has(prop) {
-  return function(obj) {
-    return prop in obj
-  }
-}
-
 export function set(key, value, obj) {
   const clone = Object.assign({}, obj)
   clone[key] = value
